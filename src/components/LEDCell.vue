05c4b82156932867998c36a4df48a59b97bf8384
ugis70194 <ugis70194@yahoo.co.jp>
2022-05-28 11:56:43 +0900

    add LEDCell.vue

---
+++ src/components/LEDCell.vue
@@ -0,0 +1,27 @@
+<template>
+  <div v-bind:class="{on: ON}">
+    cell
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LEDCell',
+  props: {
+    ON: Boolean
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+div {
+  background-color: lightgray;
+  height: 60px;
+}
+
+.on {
+  background-color: hotpink;
+}
+</style>
