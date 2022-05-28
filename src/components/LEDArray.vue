25f49b9ee716ba530b1b36bc99530bb4e45efadd
ugis70194 <ugis70194@yahoo.co.jp>
2022-05-28 11:56:55 +0900

    LEDArray.vue

---
+++ src/components/LEDArray.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wrapper">
+    <LEDCell v-bind:ON="true" />
+    <LEDCell v-bind:ON="true" />
+    <LEDCell v-bind:ON="true" />
+    <LEDCell v-bind:ON="true" />
+    <LEDCell v-bind:ON="true" />
+    <LEDCell v-bind:ON="true" />
+    <LEDCell v-bind:ON="true" />
+    <LEDCell v-bind:ON="true" />
+    <LEDCell v-bind:ON="true" />
+    <LEDCell v-bind:ON="true" />
+    <LEDCell v-bind:ON="true" />
+    <LEDCell v-bind:ON="true" />
+    <LEDCell v-bind:ON="true" />
+    <LEDCell v-bind:ON="true" />
+    <LEDCell v-bind:ON="true" />
+    <LEDCell v-bind:ON="true" />
+  </div>
+</template>
+
+<script>
+import LEDCell from './LEDCell.vue'
+
+export default {
+  name: 'LEDArray',
+  components: {
+    LEDCell
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.wrapper {
+  margin-left: auto;
+  margin-right: auto;
+  display: grid;
+  grid-template-columns: 60px 60px 60px 60px 60px 60px 60px 60px 60px 60px 60px 60px 60px 60px 60px 60px;
+  width: 960px;
+}
+</style>
