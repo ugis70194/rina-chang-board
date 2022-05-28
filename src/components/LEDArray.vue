51ed12153cab4de4bb3469a2661dce3a358a66f9
ugis70194 <ugis70194@yahoo.co.jp>
2022-05-28 17:26:12 +0900

    LEDMatrixから送られてきたデータをLEDCellに中継

--- src/components/LEDArray.vue
+++ src/components/LEDArray.vue
@@ -1,21 +1,29 @@
 <template>
   <div class="wrapper">
-    <LEDCell v-bind:ON="true" />
-    <LEDCell v-bind:ON="true" />
-    <LEDCell v-bind:ON="true" />
-    <LEDCell v-bind:ON="true" />
-    <LEDCell v-bind:ON="true" />
-    <LEDCell v-bind:ON="true" />
-    <LEDCell v-bind:ON="true" />
-    <LEDCell v-bind:ON="true" />
-    <LEDCell v-bind:ON="true" />
-    <LEDCell v-bind:ON="true" />
-    <LEDCell v-bind:ON="true" />
-    <LEDCell v-bind:ON="true" />
-    <LEDCell v-bind:ON="true" />
-    <LEDCell v-bind:ON="true" />
-    <LEDCell v-bind:ON="true" />
-    <LEDCell v-bind:ON="true" />
+    <LEDCell :ON="stateArray[0]" />
+    <LEDCell :ON="stateArray[1]" />
+    <LEDCell :ON="stateArray[2]" />
+    <LEDCell :ON="stateArray[3]" />
+    <LEDCell :ON="stateArray[4]" />
+    <LEDCell :ON="stateArray[5]" />
+    <LEDCell :ON="stateArray[6]" />
+    <LEDCell :ON="stateArray[7]" />
+    <LEDCell :ON="stateArray[8]" />
+    <LEDCell :ON="stateArray[9]" />
+    <LEDCell :ON="stateArray[10]" />
+    <LEDCell :ON="stateArray[11]" />
+    <LEDCell :ON="stateArray[12]" />
+    <LEDCell :ON="stateArray[13]" />
+    <LEDCell :ON="stateArray[14]" />
+    <LEDCell :ON="stateArray[15]" />
+    <LEDCell :ON="stateArray[16]" />
+    <LEDCell :ON="stateArray[17]" />
+    <LEDCell :ON="stateArray[18]" />
+    <LEDCell :ON="stateArray[19]" />
+    <LEDCell :ON="stateArray[20]" />
+    <LEDCell :ON="stateArray[21]" />
+    <LEDCell :ON="stateArray[22]" />
+    <LEDCell :ON="stateArray[23]" />
   </div>
 </template>
 
@@ -26,6 +34,9 @@ export default {
   name: 'LEDArray',
   components: {
     LEDCell
+  },
+  props: {
+    stateArray: Array
   }
 }
 </script>
@@ -36,7 +47,6 @@ export default {
   margin-left: auto;
   margin-right: auto;
   display: grid;
-  grid-template-columns: 60px 60px 60px 60px 60px 60px 60px 60px 60px 60px 60px 60px 60px 60px 60px 60px;
-  width: 960px;
+  grid-template-columns: 40px 40px 40px 40px 40px 40px 40px 40px 40px 40px 40px 40px 40px 40px 40px 40px 40px 40px 40px 40px 40px 40px 40px 40px;
 }
 </style>
