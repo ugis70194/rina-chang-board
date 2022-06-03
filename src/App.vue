d61c93ecc5974d57ff851dea508561967f9363d8
ugis70194 <ugis70194@yahoo.co.jp>
2022-06-03 19:07:21 +0900

    メニューを作った

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <h1> Rina chang board </h1>
-  <li>
-    <ul>a</ul>
-    <ul>b</ul>
-  </li>
+  <div class="wrapper">
+    <div class="menu"> fece select </div>
+  </div>
+  <ul class="wrapper">
+    <input placeholder="facename"/>
+    <button > save </button>
+  </ul>
   <LEDMatrix :faceData="this.faceData" v-on:changeState="changeState"/>
 </template>
 
@@ -46,6 +49,40 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.wrapper {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+}
+
+.menu {
+  width: 60%;
+  height: 2rem;
+  font-size: 1.5rem;
+  background-color: black;
+  color: white;
+  justify-self: center;
+}
+
+.menu:hover {
+  background-color: gray;
+}
+
+ul input {
+  height: 2rem;
+  width: 45%;
+  font-size: 1.5rem;
+}
+
+ul button {
+  height: 2rem;
+  width: 15%;
+  font-size: 1.5rem;
+}
+
+
 </style>
